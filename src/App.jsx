a647be369c2eccbaffcc5cfcd3a28b1f2824cdc7
maneeshaYasinth <maneeshayasinthgunarathna@gmail.com>
2024-08-31 12:02:03 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <h1 className='flex justify-center align-center text-2xl'>weathero</h1>
+  )
+}
+
+export default App
