fe280c0a2d7e7c99b84acc070c8b09e265d9d96e
maneeshaYasinth <maneeshayasinthgunarathna@gmail.com>
2024-09-03 01:35:11 +0530

    Added social links

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { Fade as Hamburger } from 'hamburger-react';
+import { SocialIcon } from 'react-social-icons'
 
 const NavBar = () => {
     const [isOpen, setOpen] = useState(false);
@@ -10,10 +11,10 @@ const NavBar = () => {
             {isOpen && (
                 <div className="absolute mt-16  text-black font-bold flex flex-col justify-center " >
                     <ul className="flex flex-col items-start space-y-14 p-6"> {/* Space between items */}
-                        <li className="flex items-center justify-center w-20 h-20 rounded-full  backdrop-blur"><a href="#home">Home</a></li>
-                        <li className="flex items-center justify-center w-20 h-20 rounded-full  backdrop-blur"><a href="#about">About</a></li>
-                        <li className="flex items-center justify-center w-20 h-20 rounded-full  backdrop-blur"><a href="#services">Services</a></li>
-                        <li className="mb-0 flex items-center justify-center w-20 h-20 rounded-full  backdrop-blur"><a href="#contact">Contact</a></li>
+                        <li className="flex items-center justify-center w-20 h-20 rounded-full  backdrop-blur"><SocialIcon url="https://www.linkedin.com/in/maneesha-yasinth/" /></li>
+                        <li className="flex items-center justify-center w-20 h-20 rounded-full  backdrop-blur"><SocialIcon url="https://github.com/maneeshaYasinth" /></li>
+                        <li className="flex items-center justify-center w-20 h-20 rounded-full  backdrop-blur"><SocialIcon url="https://www.instagram.com/maneesha_yasinth/" /></li>
+                        <li className="mb-0 flex items-center justify-center w-20 h-20 rounded-full  backdrop-blur"><SocialIcon url="https://web.facebook.com/maneesha.gunarathna.7" /></li>
                     </ul>
                 </div>
             )}
