fb1dacad440d50b36a83e48dc0e2d62f7c134ab7
maneeshaYasinth <maneeshayasinthgunarathna@gmail.com>
2024-09-03 01:24:04 +0530

    Added new sidebar

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -8,12 +8,12 @@ const NavBar = () => {
         <nav className="fixed">
             <Hamburger color="#4FD1C5" toggled={isOpen} toggle={setOpen} />
             {isOpen && (
-                <div className="absolute mt-32  bg-gray-500 text-black font-bold flex flex-col justify-center backdrop-blur"style={{ backdropFilter: 'blur(10px)', borderRadius: '0.5rem' }} >
-                    <ul className="flex flex-col items-start space-y-28 p-6"> {/* Space between items */}
-                        <li className=""><a href="#home">Home</a></li>
-                        <li><a href="#about">About</a></li>
-                        <li><a href="#services">Services</a></li>
-                        <li className="mb-0"><a href="#contact">Contact</a></li>
+                <div className="absolute mt-16  text-black font-bold flex flex-col justify-center " >
+                    <ul className="flex flex-col items-start space-y-14 p-6"> {/* Space between items */}
+                        <li className="flex items-center justify-center w-20 h-20 rounded-full  backdrop-blur"><a href="#home">Home</a></li>
+                        <li className="flex items-center justify-center w-20 h-20 rounded-full  backdrop-blur"><a href="#about">About</a></li>
+                        <li className="flex items-center justify-center w-20 h-20 rounded-full  backdrop-blur"><a href="#services">Services</a></li>
+                        <li className="mb-0 flex items-center justify-center w-20 h-20 rounded-full  backdrop-blur"><a href="#contact">Contact</a></li>
                     </ul>
                 </div>
             )}
